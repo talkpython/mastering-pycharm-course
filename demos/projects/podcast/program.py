3f87ffc83547dd32f2457d7146acb6a148cd3787
Michael Kennedy <mikeckennedy@gmail.com>
2017-11-30 11:32:33 -0800

    Factored into methods.

--- demos/projects/podcast/program.py
+++ demos/projects/podcast/program.py
@@ -2,11 +2,19 @@ import service
 
 
 def main():
-    print("Welcome to the talk python info downloader.")
-    print()
+    show_header()
 
     service.download_info()
 
+    display_results()
+
+
+def show_header():
+    print("Welcome to the talk python info downloader.")
+    print()
+
+
+def display_results():
     for show_id in range(120, 141):
         info = service.get_episode(show_id)
         print("{}. {}".format(info.show_id, info.title))
