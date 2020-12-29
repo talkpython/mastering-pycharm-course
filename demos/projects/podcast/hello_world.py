889f359bc62fe531b49c58fbbc0ceafe620699c9
saryamane <samirarya@gmail.com>
2020-12-29 10:35:24 -0800

    Added some more course files to the demos project.

---
+++ demos/projects/podcast/hello_world.py
@@ -0,0 +1,13 @@
+def hello(name: str) -> None:
+    print("Hello", name)
+
+
+def list_range(num_list: list) -> None:
+    for i, num in enumerate(num_list):
+        print("{} :- {}".format(i, num))
+
+
+if __name__ == '__main__':
+    hello('Chinu')
+    hello('Samir')
+    list_range([0, 1, 1, 2, 3, 5, 8, 13])
