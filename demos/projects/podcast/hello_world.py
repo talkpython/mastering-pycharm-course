8bfa518b9034b71033c5bf8dd3e581cc31ae4ec1
saryamane <samirarya@gmail.com>
2020-12-29 12:33:29 -0800

    Added a test function

--- demos/projects/podcast/hello_world.py
+++ demos/projects/podcast/hello_world.py
@@ -6,8 +6,13 @@ def list_range(num_list: list) -> None:
     for i, num in enumerate(num_list):
         print("{} :- {}".format(i, num))
 
+def this_test(name='clear'):
+    print(name)
+
 
 if __name__ == '__main__':
     hello('Chinu')
     hello('Samir')
+    this_test(name='Chinku')
     list_range([0, 1, 1, 2, 3, 5, 8, 13, 21])
+
