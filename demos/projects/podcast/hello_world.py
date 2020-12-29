1e3e7c208219b94c1103817047cb58243f247339
saryamane <samirarya@gmail.com>
2020-12-29 10:40:44 -0800

    Added more to the list

--- demos/projects/podcast/hello_world.py
+++ demos/projects/podcast/hello_world.py
@@ -10,4 +10,4 @@ def list_range(num_list: list) -> None:
 if __name__ == '__main__':
     hello('Chinu')
     hello('Samir')
-    list_range([0, 1, 1, 2, 3, 5, 8, 13])
+    list_range([0, 1, 1, 2, 3, 5, 8, 13, 21])
