0431c62287107e3fe3b915d8875977e19daa1940
Michael Kennedy <mikeckennedy@gmail.com>
2021-06-11 09:44:38 -0700

    Starter code for code with me podcast demo.

---
+++ code/code_with_me/podcast/service.py
@@ -0,0 +1,40 @@
+from collections import namedtuple
+from xml.etree import ElementTree
+
+import requests
+
+Episode = namedtuple('Episode', 'title link pubdate show_id')
+episode_data = {}
+
+
+def download_info():
+    url = 'https://pythonbytes.fm/episodes/rss'
+
+    resp = requests.get(url)
+    resp.raise_for_status()
+
+    dom = ElementTree.fromstring(resp.text)
+
+    items = dom.findall('channel/item')
+    episode_count = len(items)
+
+    for idx, item in enumerate(items):
+        episode = Episode(
+            item.find('title').text,
+            item.find('link').text,
+            item.find('pubDate').text,
+            episode_count - idx - 1
+        )
+        episode_data[episode.show_id] = episode
+
+
+def get_episode(show_id: int) -> Episode:
+    return episode_data.get(show_id)
+
+
+def get_latest_show_id():
+    return max(episode_data.keys())
+
+
+def get_min_show_id():
+    return min(episode_data.keys())
