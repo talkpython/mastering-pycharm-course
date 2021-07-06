f48e2f49e659a1aadf4c834c081e71be7b519cf8
Michael Kennedy <mikeckennedy@gmail.com>
2021-07-05 17:40:16 -0700

    Hello world, but with external requirments.

--- demos/projects/first_project/hello.py
+++ demos/projects/first_project/hello.py
@@ -1,1 +1,10 @@
-print("hello world")
+import requests
+# import sqlalchemy
+
+resp = requests.get('https://weather.talkpython.fm/api/weather?city=portland&state=OR&country=US&units=imperial')
+resp.raise_for_status()
+
+data = resp.json()
+temp = data['forecast']['temp']
+
+print(f"hello world, it's {temp} outside!")
