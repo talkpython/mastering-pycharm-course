18427fe0c00cc5b8233896402fcfc4034fbf1e37
Michael Kennedy <mikeckennedy@gmail.com>
2021-07-30 18:00:15 -0700

    Fixes #11 - refactored display method.

--- demos/projects/podcast/program.py
+++ demos/projects/podcast/program.py
@@ -18,8 +18,12 @@ def show_titles():
     end = service.get_max_episode_id()
 
     for episode_id in range(start, end + 1):
-        episode = service.get_details(episode_id)
-        print(f'{episode.id}. -> {episode.title}')
+        show_episode_details(episode_id)
+
+
+def show_episode_details(episode_id: int):
+    episode = service.get_details(episode_id)
+    print(f'{episode.id}. -> {episode.title}')
 
 
 if __name__ == '__main__':
