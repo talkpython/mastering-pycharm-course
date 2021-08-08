9ddc5326180a1f0a512bdd632eb2dea0c993dd89
Michael Kennedy <mikeckennedy@gmail.com>
2021-08-08 16:27:54 -0700

    Showing recent tweets, sort of.

--- demos/projects/web_serverside/first_flask/app.py
+++ demos/projects/web_serverside/first_flask/app.py
@@ -2,6 +2,7 @@ import random
 
 import flask
 from flask import Flask
+from data.tweets import Tweet
 
 app = Flask(__name__)
 
@@ -10,6 +11,12 @@ app = Flask(__name__)
 def index():
     num = random.randint(50, 100)
     return flask.render_template('home/index.html', number=num)
+
+
+@app.route('/tweets')
+def tweets():
+    recent_tweets = Tweet.load()
+    return flask.render_template('home/tweets.html', recent_tweets=recent_tweets)
 
 
 if __name__ == '__main__':
