0106b5655f07b3ba3663e77f99ea80228bb3b1a6
Michael Kennedy <mikeckennedy@gmail.com>
2021-08-09 14:48:34 -0700

    Basic js+ts+html done.

---
+++ demos/projects/web_clientside/basic_clientside_web/person.ts
@@ -0,0 +1,7 @@
+
+class Person {
+    constructor(public firstName: string,
+                public lastName: string,
+                public age: number) {
+    }
+}
--- demos/projects/web_clientside/basic_clientside_web/site.js
+++ demos/projects/web_clientside/basic_clientside_web/site.js
@@ -1,8 +1,9 @@
-
 function runAtPageLoad(message) {
-    let person = "Sarah";
+    //let person = "Sarah";
+    let person = new Person('Sarah', 'James', 34)
 
-    console.log(person + ' --> Running at startup: ' + message);
+    console.log(person.firstName + ' (age: ' + person.age +
+        ') --> Running at startup: ' + message);
 }
 
 runAtPageLoad("This is the startup message!");
