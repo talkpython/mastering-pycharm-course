8e77d928460953fba308170da66ae6795d25308a
Michael Kennedy <mikeckennedy@gmail.com>
2021-08-14 12:06:36 -0700

    Improved the perf of compute/ML side of our app.

--- demos/projects/performance/core_compute.py
+++ demos/projects/performance/core_compute.py
@@ -1,3 +1,4 @@
+import functools
 import math
 import time
 
@@ -12,7 +13,9 @@ def compute_analytics(search, rows):
 
 def read_data(data):
     for _ in range(0, len(data)):
-        time.sleep(.02)
+        # Imagine: We switch to NumPy, gain 20x
+        # time.sleep(.02)
+        time.sleep(.001)
 
     return data
 
@@ -23,12 +26,19 @@ def learn(search_data, db_data):
         for idd, d in enumerate(db_data):
             for r in range(1, 100):
                 val1 = ids * idd * len(s)
-                val2 = math.pow(idd, 7)
+                # val2 = math.pow(idd, 7)
+                val2 = compute_pow(idd, 7)
 
                 res = math.sqrt(val1 * val1 + val2 * val2)
-                mod = math.pow(-1, ids + r)
+                # mod = math.pow(-1, ids + r)
+                mod = compute_pow(-1, ids + r)
 
                 total += res * mod
                 total = total
 
     return total
+
+
+@functools.lru_cache()
+def compute_pow(x, y):
+    return math.pow(x, y)
