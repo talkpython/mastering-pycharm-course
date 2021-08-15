fb13773390072d5c24db843e4f5203ebf3820004
Michael Kennedy <mikeckennedy@gmail.com>
2021-08-15 12:01:09 -0700

    Let's make the demo testing app a little more tolerant of bad inputs.

--- demos/projects/unittesting/program.py
+++ demos/projects/unittesting/program.py
@@ -3,7 +3,9 @@ import core
 
 def main():
     while True:
-        book_event()
+        if not book_event():
+            break
+
         print()
 
 
@@ -19,6 +21,10 @@ def book_event():
     print("3. Seafood")
     print("4. Other")
     choice = input("Choose a number: ")
+    if not choice or not choice.strip():
+        print("KTHXBYE")
+        return False
+
     choice = int(choice)
 
     options = core.find_available(choice)
@@ -26,18 +32,24 @@ def book_event():
     print()
     if not options:
         print("Whoops, no tables for that food type. Try another.")
-        return
+        return True
 
     print("Here are the open tables:")
     for idx, o in enumerate(options):
         print("{}. Restaurant: {}, table: {}.".format(idx + 1, o.restaurant, o.table_id))
 
     print()
-    num = int(input("Enter a number of the table to book: ")) - 1
+    choice = input("Enter a number of the table to book: ")
+    if not choice or not choice.strip():
+        print("KTHXBYE")
+        return False
+
+    num = int(choice) - 1
 
     to_book = options[num]
     booked = core.book_table(to_book.table_id)
     print("Booked you a table at {} in table {}".format(booked.restaurant, booked.table_id))
+    return True
 
 
 if __name__ == '__main__':
