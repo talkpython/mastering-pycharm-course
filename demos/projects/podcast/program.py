b6391267b2ec717808f0990fdcd39629ebf0e87d
Dan <dangraysf@gmail.com>
2022-01-23 11:28:51 -0500

    mutliple lines commented out

--- demos/projects/podcast/program.py
+++ demos/projects/podcast/program.py
@@ -19,13 +19,14 @@ def show_titles():
     start = service.get_min_episode_id()
     end = service.get_max_episode_id()
 
-    for episode_id in range(start, end + 1):
-        show_episode_details(episode_id)
+    # for episode_id in range(start, end + 1):
+    #     show_episode_details(episode_id)
 
 
-def show_episode_details(episode_id: int):
-    episode = service.get_details(episode_id)
-    print(f'{episode.id}. -> {episode.title}')
+# def show_episode_details(episode_id: int):
+#     episode = service.get_details(episode_id)
+#     print(f'{episode.id}. -> {episode.title}')
+
 
 
 if __name__ == '__main__':
