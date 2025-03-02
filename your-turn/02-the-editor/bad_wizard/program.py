f7a0f8a54fc510dfffec2a22bb9c5e68dd4cc270
Michael Kennedy <mikeckennedy@gmail.com>
2025-03-02 10:28:51 -0800

    A few tweaks for the editor section.

--- your-turn/02-the-editor/bad_wizard/program.py
+++ your-turn/02-the-editor/bad_wizard/program.py
@@ -1,5 +1,7 @@
 import time
 import os
+import sys
+import functools
 import datetime
 import random
 
@@ -8,11 +10,9 @@ def main():
     print_header()
     game_loop()
 def print_header():
-    # Yes, I added this after I recorded the video
-    # but I thought you'd get a kick out if it. ;)
     print()
     print('-----------------------------------------------------------------------')
-    print('''
+    print(r'''
    (  )   /\   _                 (
     \ |  (  \ ( \.(               )                      _____
   \  \ \  `  `   ) \             (  ___                 / _   \\
@@ -28,33 +28,32 @@ def print_header():
    '  (__/                                             /                  \\
     ''')
     print()
-    print('-----------------------------------------------------------------------')
+    print(     '-----------------------------------------------------------------------')
     print()
 def game_loop():
     creatures = [
             SmallAnimal( 'Toad',1 ),
-            Creature( 'Tiger',12 ),
-            SmallAnimal('Bat',3 ),
-            Dragon('Dragon',50,75,True),
+               Creature( 'Tiger',12 ),
+               SmallAnimal('Bat',3 ),
+               Dragon('Dragon',50,75,True),
         Wizard('Evil Wizard',1000)
     ]
 
     hero = Wizard('Gandolf', 75)
 
-
     while True:
 
-        active_creature = random.choice(creatures)
-        print('A {} of level {} has appear from a dark and foggy forest...'
+        active_creature = random.choice(   creatures    )
+        print( 'A {} of level {} has appear from a dark and foggy forest...'
                   .format(active_creature.name, active_creature.level))
         print()
 
-        cmd = input('Do you [a]ttack, [r]unaway, or [l]ook around? ')
-        if cmd == 'a':
-            if hero.attack(active_creature):
+        cmd = input( 'Do you [a]ttack, [r]unaway, or [l]ook around? '                )
+        if cmd=='a':
+            if hero.attack(active_creature) :
                 creatures.remove(active_creature)
-            else:
-                print("The wizard runs and hides taking time to recover...")
+            else    :
+                print( "The wizard runs and hides taking time to recover..." )
                 time.sleep(5)
                 print("The wizard returns revitalized!")
         elif cmd == 'r':
@@ -68,10 +67,32 @@ def game_loop():
             print("OK, exiting game... bye!")
             break
 
+
+
+
+
+
+
+
+
+
+
+
         if not creatures:
             print("You've defeated all the creatures, well done!")
             break
 
+
+
+
+
         print()
-if __name__ == '__main__':
+if __name__     ==    '__main__':
     main()
+
+
+
+
+
+
+
