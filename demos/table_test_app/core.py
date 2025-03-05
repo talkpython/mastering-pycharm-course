627a49bce1aed67c1fe854f75387746a76bed8b7
Michael Kennedy <mikeckennedy@gmail.com>
2025-03-04 19:40:12 -0800

    minor code cleanup

--- demos/table_test_app/core.py
+++ demos/table_test_app/core.py
@@ -21,20 +21,16 @@ class TableUnavailableError(Exception):
 def find_available(choice: int) -> List[Table]:
     global tables
 
-    return [
-        t
-        for t in tables
-        if t.type == choice and not t.is_booked
-    ]
+    return [t for t in tables if t.type == choice and not t.is_booked]
 
 
 def book_table(table_id: str):
     table = find_table_by_id(table_id)
     if not table:
-        raise EntityNotFoundError("No table with this id")
+        raise EntityNotFoundError('No table with this id')
 
     if table.is_booked:
-        raise TableUnavailableError("Table is booked")
+        raise TableUnavailableError('Table is booked')
 
     table.is_booked = True
     return table
@@ -52,28 +48,20 @@ def _id():
     return str(uuid.uuid4())
 
 
-tables= [
-    Table(_id(), "Thai Roses", 1),
-    Table(_id(), "Thai Roses", 1),
-
-    Table(_id(), "Siam Thai", 1),
-
-    Table(_id(), "Skyline Burgers", 2),
-
-    Table(_id(), "Little Big Burger", 2),
-    Table(_id(), "Little Big Burger", 2),
-    Table(_id(), "Little Big Burger", 2),
-
+tables = [
+    Table(_id(), 'Thai Roses', 1),
+    Table(_id(), 'Thai Roses', 1),
+    Table(_id(), 'Siam Thai', 1),
+    Table(_id(), 'Skyline Burgers', 2),
+    Table(_id(), 'Little Big Burger', 2),
+    Table(_id(), 'Little Big Burger', 2),
+    Table(_id(), 'Little Big Burger', 2),
     Table(_id(), "Jake's Seafood", 3),
     Table(_id(), "Jake's Crawfish", 3),
-    Table(_id(), "Portland City Grill", 3),
-
-    Table(_id(), "Waffle Window", 4),
-    Table(_id(), "Slappy Cakes", 4),
-    Table(_id(), "Slappy Cakes", 4),
+    Table(_id(), 'Portland City Grill', 3),
+    Table(_id(), 'Waffle Window', 4),
+    Table(_id(), 'Slappy Cakes', 4),
+    Table(_id(), 'Slappy Cakes', 4),
 ]
 
-table_lookup = {
-    t.table_id: t
-    for t in tables
-}
+table_lookup = {t.table_id: t for t in tables}
--- demos/table_test_app/tests.py
+++ demos/table_test_app/tests.py
@@ -1,11 +1,12 @@
 import pytest
+
 import core
 
 
 def test_there_are_available_tables():
     choice = 1
     one_tables = core.find_available(choice)
-    assert (len(one_tables))
+    assert len(one_tables)
 
 
 def test_table_can_be_booked():
@@ -18,10 +19,9 @@ def test_table_can_be_booked():
     assert table.table_id == booked_table.table_id
 
 
-
 def test_cannot_book_nonexistant_table():
     with pytest.raises(core.EntityNotFoundError):
-        core.book_table("not-a-table-id")
+        core.book_table('not-a-table-id')
 
 
 def test_cannot_book_booked_table():
